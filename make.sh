c37c4f8aaaf6f7070096c3135787f86d478d1ef5
Edwin Steele <edwin-lists@wordspeak.org>
2014-07-13 09:36:38 +1000

    Add missing link

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/Code/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux.conf vimrc vimrc_python gitconfig zshrc bashrc"
+files="vim tmux.conf vimrc vimrc_python gitconfig zshrc bashrc"
 
 ##########
 
