2a342c496c3285fdaface52e2303885452f78562
Edwin Steele <edwin@wordspeak.org>
2019-01-09 10:18:15 +1100

    Map possible storm to character

--- btt-weather.sh
+++ btt-weather.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Local Temperature summary from BOM. Consumed by BetterTouchTool for display in the touchbar
-APP_TEMP=$(curl -s -H "Accept-Encoding: gzip" -s http://www.bom.gov.au/fwo/IDN60801/IDN60801.94763.json | 
+APP_TEMP=$(curl -H "Accept-Encoding: gzip" -s http://www.bom.gov.au/fwo/IDN60801/IDN60801.94763.json |
 	gzip -d 2> /dev/null |
        	/usr/local/bin/jq '.observations.data[0].apparent_t')
 /bin/echo -n "${APP_TEMP}°C "
@@ -16,7 +16,7 @@ case $FORECAST_ICON_NUMBER in
   3)		ICON="⛅️";;  # Mostly sunny/partly cloudy
   4)		ICON="☁️";;  # Cloudy
   9)		ICON="🌧💨";;  # Showers, very windy
-  11|17)	ICON="🌦";;  # Possible Shower/Shower or two
+  11|16|17)	ICON="🌦";;  # Possible Shower/Shower or two
   18)		ICON="🌧";;  # Rain
   *)            ICON="🐪 ($FORECAST_ICON_NUMBER)";;  # Not sure
 esac
